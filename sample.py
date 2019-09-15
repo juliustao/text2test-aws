ae976b25f47327c40f977533ed1af0ca75bd8a69
dragotao <juliusltao@gmail.com>
2019-09-14 23:21:52 -0400

    sample paragraph that would be inputted

---
+++ sample.py
@@ -0,0 +1,17 @@
+sample = """
+Britain’s two main political parties are haggling over the nation’s withdrawal from the European Union, known as Brexit. The badly divided government is in crisis, unable to agree on an approach to the country’s biggest peacetime decision in decades. The deadline to come up with a plan is fast approaching.
+
+The struggle has already cost one prime minister, Theresa May, her job; she announced on May 24 that she would resign after failing to come up with a plan that satisfied her party, her coalition partners and officials in Brussels, the seat of the European Union.
+
+Now it is taking a toll on her successor, Boris Johnson. The Conservative Party chose Mr. Johnson, a brash proponent of withdrawal, to succeed Mrs. May and take control of the Brexit process.
+
+It has not gone well.
+
+Many lawmakers were outraged over Mr. Johnson’s insistence that if need be, he would pull Britain from the European Union even without a formal agreement — a move many warn could mean major economic damage. And his attempts to in effect shut Parliament out of the process did not win him many friends.
+
+When lawmakers rose up, seizing control of the legislative process, and Mr. Johnson lost his majority in Parliament, he responded by tossing out rebels from his Conservative Party and demanding a new general election.
+
+The deadline for withdrawal is currently Oct. 31. Mr. Johnson insists he can cut a deal with the European Union before then. But time is rapidly running out.
+
+What ultimately emerges could determine the shape of Britain and its place in the world for decades.
+"""
