f4909ca21a5d6cf71af44bdfed66a66598626efd
dragotao <juliusltao@gmail.com>
2019-09-15 09:19:47 -0400

    add better sentence splitter function

--- comprehend.py
+++ comprehend.py
@@ -1,32 +1,57 @@
 import boto3
+import re
 
 MIN_SCORE = 0.85
-DELIMITERS = ['.', '?', '!', '\n']
+
+alphabets= "([A-Za-z])"
+prefixes = "(Mr|St|Mrs|Ms|Dr|Prof|Capt|Cpt|Lt|Mt)[.]"
+suffixes = "(Inc|Ltd|Jr|Sr|Co)"
+starters = "(Mr|Mrs|Ms|Dr|He\s|She\s|It\s|They\s|Their\s|Our\s|We\s|But\s|However\s|That\s|This\s|Wherever)"
+acronyms = "([A-Z][.][A-Z][.](?:[A-Z][.])?)"
+websites = "[.](com|net|org|io|gov|me|edu)"
+digits = "([0-9])"
+months = "(Jan|Feb|Mar|Apr|Jun|Jul|Aug|Sep|Oct|Nov|Dec)[.]"
 
 client = boto3.client('comprehend')
+
+
+def split_into_sentences(text):
+    text = " " + text + "  "
+    text = text.replace("\n"," ")
+    text = re.sub(prefixes,"\\1<prd>",text)
+    text = re.sub(months, "\\1<prd>", text)
+    text = re.sub(websites,"<prd>\\1",text)
+    text = re.sub(digits + "[.]" + digits, "\\1<prd>\\2", text)
+    if "Ph.D" in text: text = text.replace("Ph.D.","Ph<prd>D<prd>")
+    if "e.g." in text: text = text.replace("e.g.", "e<prd>g<prd>")
+    if "i.e." in text: text = text.replace("i.e.", "i<prd>e<prd>")
+    if "..." in text: text = text.replace("...", "<prd><prd><prd>")
+    text = re.sub("\s" + alphabets + "[.] "," \\1<prd> ",text)
+    text = re.sub(acronyms+" "+starters,"\\1<stop> \\2",text)
+    text = re.sub(alphabets + "[.]" + alphabets + "[.]" + alphabets + "[.]","\\1<prd>\\2<prd>\\3<prd>",text)
+    text = re.sub(alphabets + "[.]" + alphabets + "[.]","\\1<prd>\\2<prd>",text)
+    text = re.sub(" "+suffixes+"[.] "+starters," \\1<stop> \\2",text)
+    text = re.sub(" "+suffixes+"[.]"," \\1<prd>",text)
+    text = re.sub(" " + alphabets + "[.]"," \\1<prd>",text)
+    if "”" in text: text = text.replace(".”","”.")
+    if "\"" in text: text = text.replace(".\"","\".")
+    if "!" in text: text = text.replace("!\"","\"!")
+    if "?" in text: text = text.replace("?\"","\"?")
+    text = text.replace(".",".<stop>")
+    text = text.replace("?","?<stop>")
+    text = text.replace("!","!<stop>")
+    text = text.replace("<prd>",".")
+    sentences = text.split("<stop>")
+    sentences = sentences[:-1]
+    sentences = [s.strip() for s in sentences]
+    return sentences
 
 
 def lambda_handler(event, context):
     results = []
 
     input_text = event['inputTranscript']
-    index_list = [0]
-    for i in range(len(input_text)):
-        if input_text[i] in DELIMITERS:
-            index_list.append(i+1)
-
-    if len(index_list) <= 1:
-        return results
-
-    list_of_input_sentences = []
-    for i in range(len(index_list) - 1):
-        sentence = input_text[index_list[i]:index_list[i+1]].strip(' ”"\')')
-        if len(sentence) > 2:
-            list_of_input_sentences.append(sentence)
-    # splits up input text into sentences
-
-    if len(list_of_input_sentences) <= 0:
-        return results
+    list_of_input_sentences = split_into_sentences(input_text)
 
     list_of_entities_in_sentences = client.batch_detect_entities(TextList=list_of_input_sentences, LanguageCode='en')['ResultList']
     # type(sentence_entities_list): list of dictionaries
